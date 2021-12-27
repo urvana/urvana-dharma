4a1d9770f4b15ef1928c672e35d27581bb70186c
Patricio López Juri <lopezjuripatricio@gmail.com>
2021-12-27 11:55:16 -0300

    feat: Social links

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { ExternalLinkIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MailIcon } from '@heroicons/react/solid'
 
 const navigation = [
   { name: 'Proyectos', href: '#' },
@@ -11,7 +12,7 @@ const navigation = [
   // { name: 'Company', href: '#' },
 ]
 
-export default function Home() {
+export default function RootPage() {
   return (
     <div className="">
       <Head>
@@ -62,7 +63,7 @@ export default function Home() {
                       {item.name}
                     </a>
                   ))}
-                  <a href={"https://app.urvana.net/formulario/dharma"} className="font-medium text-blue-600 hover:text-blue-500 inline-flex items-center space-x-1">
+                  <a href={"https://urvana.net/formulario/dharma"} className="font-medium text-blue-600 hover:text-blue-500 inline-flex items-center space-x-1">
                     <span>
                       Ingresar predios
                     </span>
@@ -113,7 +114,7 @@ export default function Home() {
                     ))}
                   </div>
 
-                  <a href={"https://app.urvana.net/formulario/dharma"} className="w-full px-5 py-3 text-center font-medium text-blue-600 bg-gray-50 hover:bg-gray-100 flex items-center space-x-1">
+                  <a href={"https://urvana.net/formulario/dharma"} className="w-full px-5 py-3 text-center font-medium text-blue-600 bg-gray-50 hover:bg-gray-100 flex items-center space-x-1">
                     <span>
                       Ingresar predios
                     </span>
@@ -136,7 +137,7 @@ export default function Home() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://www.urvana.net"
+                    href="https://urvana.net?ref=demo"
                     {...EXTERNAL({ follow: true })}
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
                   >
@@ -145,7 +146,7 @@ export default function Home() {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href={"https://app.urvana.net/formulario/dharma"}
+                    href={"https://urvana.net/formulario/dharma"}
                     {...EXTERNAL({ follow: true })}
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
                   >
@@ -172,7 +173,7 @@ export default function Home() {
         <p className="text-gray-500 text-base text-center">
           En{" "}
           <a
-            href={`https://www.urvana.net?ref=${"demo"}`}
+            href={`https://urvana.net?ref=demo`}
             {...EXTERNAL({ follow: true })}
             className="text-blue-700 hover:text-blue-900 transition-all"
           >
@@ -181,7 +182,7 @@ export default function Home() {
           estamos modernizando la provisión de terrenos para desarrollo inmobiliario.
         </p>
         <div className="flex space-x-6">
-          {NAVIGATION.map((item) => (
+          {FOOTER_SOCIAL.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
@@ -194,10 +195,27 @@ export default function Home() {
   )
 }
 
-const NAVIGATION = [
+
+/* eslint-disable @typescript-eslint/no-shadow */
+
+/** Helper for external links */
+export function EXTERNAL({ referrer = true, opener = false, follow = false } = {}) {
+  const rel = [];
+  if (referrer === false) rel.push("noreferrer");
+  if (opener === false) rel.push("noopener");
+  if (follow === false) rel.push("nofollow");
+  return { target: "_blank", rel: rel.join(" ") } ;
+}
+
+export const HREF_INSTAGRAM = "https://instagram.com/urvanachile";
+export const HREF_TWITTER = "https://twitter.com/urvanachile";
+export const HREF_FACEBOOK = "https://facebook.com/urvanachile";
+export const HREF_GITHUB = "https://github.com/urvana";
+
+export const FOOTER_SOCIAL = [
   {
     name: "Facebook",
-    href: "#",
+    href: HREF_FACEBOOK,
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -210,7 +228,7 @@ const NAVIGATION = [
   },
   {
     name: "Instagram",
-    href: "#",
+    href: HREF_INSTAGRAM,
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -223,7 +241,7 @@ const NAVIGATION = [
   },
   {
     name: "Twitter",
-    href: "#",
+    href: HREF_TWITTER,
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
@@ -231,46 +249,40 @@ const NAVIGATION = [
     ),
   },
   {
-    name: "LinkedIn",
-    href: "#",
-    icon: (props) => (
-      // https://iconmonstr.com/
-      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-      </svg>
-    ),
-  },
-  {
-    name: "WhatsApp",
-    href: "#",
+    name: "GitHub",
+    href: HREF_GITHUB,
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-        <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
       </svg>
     ),
   },
   // {
-  //   name: "Email",
+  //   name: "LinkedIn",
   //   href: "#",
   //   icon: (props) => (
+  //     // https://iconmonstr.com/
   //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-  //       <path
-  //         fillRule="evenodd"
-  //         d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-  //         clipRule="evenodd"
-  //       />
+  //       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
   //     </svg>
   //   ),
   // },
+  // {
+  //   name: "WhatsApp",
+  //   href: "#",
+  //   icon: (props) => (
+  //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+  //       <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
+  //     </svg>
+  //   ),
+  // },
+  {
+    name: "Email",
+    href: "mailto:hola@urvana.net",
+    icon: (props) => <MailIcon fill="currentColor" {...props} />,
+  },
 ];
-
-/* eslint-disable @typescript-eslint/no-shadow */
-
-/** Helper for external links */
-export function EXTERNAL({ referrer = true, opener = false, follow = false } = {}) {
-  const rel = [];
-  if (referrer === false) rel.push("noreferrer");
-  if (opener === false) rel.push("noopener");
-  if (follow === false) rel.push("nofollow");
-  return { target: "_blank", rel: rel.join(" ") } ;
-}
